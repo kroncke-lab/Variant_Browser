d9e285c44e1d7aeca7415a4d544586e3aa722142
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-05-03 14:30:12 -0500

    testing problems with STATIC files

--- kcnh2/urls.py
+++ kcnh2/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'kcnh2'
 urlpatterns = [
-    path('penetrance_extimate_protocol', views.penetrance_estimation, name='penetrance_estimate'),
+    path('penetrance_estimate_protocol', views.penetrance_estimation, name='penetrance_estimate'),
     path('about/', views.about, name='about'),
     path('', views.display, name='main'),
     path('variantinfo/<str:hgvsc>', views.variantview, name='detail'),
