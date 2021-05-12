fedabb16e9bdf2671a430446f2f5ed3d330e69c8
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-05-12 14:56:43 -0500

    security changes

--- variantbrowser/settings.py
+++ variantbrowser/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 
+TODO USE ENVIRONMENT VARIABLES TO SET DATABASE PARAMETERS!!!
 TODO add SCN5A functionality
 """
 
