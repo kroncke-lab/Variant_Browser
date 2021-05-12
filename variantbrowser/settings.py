71da58f5a780c5305b7418401246289921d12c33
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-05-12 11:28:24 -0500

    security changes

--- variantbrowser/settings.py
+++ variantbrowser/settings.py
@@ -84,45 +84,25 @@ DATABASE_ROUTERS = ['variantbrowser.dbrouter.DemoRouter']
 DATABASE_APPS_MAPPING = {'azure_data': 'azure_sql_db'}
 
 
-if 'DJANGO_DATABASE_PASSWORD' in os.environ.keys():
-    # Staging or production database
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        },
-        'azure_sql_db': {
-            'ENGINE': 'mssql',
-            'NAME': os.environ['DJANGO_DATABASE_NAME'],
-            'USER': os.environ['DJANGO_DATABASE_USER'],
-            'PASSWORD': os.environ['DJANGO_DATABASE_PASSWORD'],
-            'HOST': os.environ['DJANGO_DATABASE_SERVER'],
-            'PORT': '1433',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'azure_sql_db': {
+        'ENGINE': 'mssql',
+        'NAME': os.environ['DJANGO_DATABASE_NAME'],
+        'USER': os.environ['DJANGO_DATABASE_USER'],
+        'PASSWORD': os.environ['DJANGO_DATABASE_PASSWORD'],
+        'HOST': os.environ['DJANGO_DATABASE_SERVER'],
+        'PORT': '1433',
 
-            'OPTIONS': {
-                'driver': 'ODBC Driver 17 for SQL Server',
-            },
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
         },
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        },
-        'azure_sql_db': {
-            'NAME': 'sandboxtest',
-            'ENGINE': 'mssql',
-            'USER': 'kronckbm',
-            'PASSWORD': 'Ch3mistry1',
-            'HOST': 'sandboxtest.database.windows.net',
-            'PORT': '1433',
+    },
+}
 
-            'OPTIONS': {
-                'driver': 'ODBC Driver 17 for SQL Server',
-            },
-        },
-    }
 
 
 
