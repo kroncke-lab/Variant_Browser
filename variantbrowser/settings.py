9311351d52bcdc976acc1fcb8755d08da4ae1c2b
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-05-12 13:15:10 -0500

    security changes

--- variantbrowser/settings.py
+++ variantbrowser/settings.py
@@ -109,19 +109,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-        },
-        'azure_sql_db': {
-            'NAME': 'sandboxtest',
-            'ENGINE': 'mssql',
-            'USER': 'kronckbm',
-            'PASSWORD': 'Ch3mistry1',
-            'HOST': 'sandboxtest.database.windows.net',
-            'PORT': '1433',
-
-            'OPTIONS': {
-                'driver': 'ODBC Driver 17 for SQL Server',
-            },
-        },
+        }
     }
 
 
