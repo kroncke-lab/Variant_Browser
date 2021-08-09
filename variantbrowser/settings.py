2ecf00b05591764c9420caa6aa29011b2a6b3640
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-08-09 14:26:13 -0500

    Security and about page update

--- variantbrowser/settings.py
+++ variantbrowser/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@p%q*!jf4nlv_r8go6$w=+p*z9@+w=%(l3w3o)@^7q7v^c61j'   # os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
