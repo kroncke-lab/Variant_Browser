d8f0153808ec755d5110c5982f9f626a7d3f9398
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2021-08-26 11:37:20 -0500

    Remove debugger

--- variantbrowser/settings.py
+++ variantbrowser/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['variantbrowser.azurewebsites.net', 'variantbrowser.org', '127.0.0.1']
 
