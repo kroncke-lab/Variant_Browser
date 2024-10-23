353d5b1252b0c7e8ae924d1d6a42b4b014ed8090
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2024-10-23 15:47:39 -0500

    Update models.py

--- kcnh2/models.py
+++ kcnh2/models.py
@@ -33,6 +33,7 @@ class newVariant(models.Model):
     beta = models.TextField(db_column='beta_revised', blank=True, null=True)
     mave_score = models.FloatField(blank=True, null=True)
     mave_score_SE = models.FloatField(blank=True, null=True)
+    mave_score_all = models.FloatField(blank=True, null=True)
     
     gnomad = models.IntegerField(db_column='gnomad_v4', blank=False, null=False, default=0)  # Field name made lowercase.
     structure = models.TextField(db_column='Structure', blank=True, null=True)  # Field name made lowercase.
