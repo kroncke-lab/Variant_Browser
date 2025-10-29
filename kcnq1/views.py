9b117e4a25766a23572cdec5b638442a9415bc16
Brett Kroncke <39835074+kroncke-lab@users.noreply.github.com>
2025-10-29 08:32:26 -0500

    Optimize variant detail queries to reduce timeouts

--- kcnq1/views.py
+++ kcnq1/views.py
@@ -1,50 +1,92 @@
 import math
+
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from kcnq1.models import KCNQ1NewVariant, KCNQ1ClinicalPapers, kcnq1Distances, KCNQ1FunctionalPapers, KCNQ1InSilico
+from kcnq1.models import (
+    KCNQ1NewVariant,
+    KCNQ1ClinicalPapers,
+    kcnq1Distances,
+    KCNQ1FunctionalPapers,
+    KCNQ1InSilico,
+)
 
 
 # Create your views here.
 # thirty days 86400 * 30
 @cache_page(60*60*31)
 def display(request):
-    recs = KCNQ1NewVariant.objects.all()
+    recs = KCNQ1NewVariant.objects.only(
+        'pos',
+        'hgvsc',
+        'var',
+        'resnum',
+        'lqt1',
+        'total_carriers',
+        'function_lqt1',
+        'structure_lqt1',
+        'lqt1_penetrance',
+    )
     ret = render(request, 'kcnq1/main.html', {'recs': recs})
     return ret
 
 
 @cache_page(60*60*31)
 def variantview(request, hgvsc):
-    resnum = KCNQ1NewVariant.objects.filter(hgvsc=hgvsc).values_list('resnum', flat=True)
-    var = KCNQ1NewVariant.objects.filter(hgvsc=hgvsc).values_list('var', flat=True)
-    recs_dists = kcnq1Distances.objects.filter(resnum=int(resnum[0]), distance__lt=15)
-    neighbors = kcnq1Distances.objects.filter(resnum=int(resnum[0])).values_list('neighbor', flat=True)
-    recs_funcs = KCNQ1FunctionalPapers.objects.filter(var=var[0])
-    recs_clin = KCNQ1ClinicalPapers.objects.filter(var=var[0], pmid__isnull=False)
-    recs_vars = KCNQ1NewVariant.objects.filter(resnum__in=neighbors, total_carriers__gt=0)
-    variant = KCNQ1NewVariant.objects.filter(var=var[0]).first()
+    variant = KCNQ1NewVariant.objects.select_related(None).only(
+        'resnum',
+        'var',
+        'total_carriers',
+        'lqt1',
+        'lqt1_penetrance',
+        'structure_lqt1',
+        'function_lqt1',
+        'gnomad',
+    ).get(hgvsc=hgvsc)
+
+    recs_dists = kcnq1Distances.objects.filter(resnum=variant.resnum, distance__lt=15)
+    neighbors = list(
+        kcnq1Distances.objects.filter(resnum=variant.resnum).values_list('neighbor', flat=True)
+    )
+    recs_funcs = KCNQ1FunctionalPapers.objects.filter(var=variant.var)
+    recs_clin = KCNQ1ClinicalPapers.objects.filter(var=variant.var, pmid__isnull=False)
+    recs_vars = KCNQ1NewVariant.objects.filter(resnum__in=neighbors).filter(
+        Q(total_carriers__gt=0) | Q(gnomad__gt=0)
+    )
     var_in_silico = KCNQ1InSilico.objects.filter(hgvsc=hgvsc).first()
 
-    #if variant.mutAA == "X" or variant.mutAA == "fsX":
-    #    var_type = False
-    #else:
     var_type = True
     unaff = variant.total_carriers - variant.lqt1
 
     try:
-        alpha = math.floor(float(variant.lqt1_penetrance*(variant.total_carriers+10)))-variant.lqt1
+        alpha = math.floor(float(variant.lqt1_penetrance * (variant.total_carriers + 10))) - variant.lqt1
         beta = 10 - alpha
         alpha_lqt1 = alpha + variant.lqt1
         tot_with_prior = 10 + variant.total_carriers
-    except ValueError and TypeError:
+    except (ValueError, TypeError):
         alpha = "NA"
         beta = "NA"
         alpha_lqt1 = "NA"
-        tot_with_prior = "MA"
-    return render(request, 'kcnq1/detail.html', {'recs_dists': recs_dists, 'recs_funcs': recs_funcs,
-                                                 'recs_clin': recs_clin, 'var': var, 'recs_vars': recs_vars,
-                                                 'variant': variant, 'alpha': alpha,
-                                                 'beta': beta, 'clin_papers': len(recs_clin),
-                                                 'var_in_silico': var_in_silico, 'func_papers': len(recs_funcs),
-                                                 'var_type': var_type, 'unaff': unaff, "alpha_lqt1":alpha_lqt1,
-                                                 "tot_with_prior":tot_with_prior})
+        tot_with_prior = "NA"
+
+    return render(
+        request,
+        'kcnq1/detail.html',
+        {
+            'recs_dists': recs_dists,
+            'recs_funcs': recs_funcs,
+            'recs_clin': recs_clin,
+            'var': [variant.var],
+            'recs_vars': recs_vars,
+            'variant': variant,
+            'alpha': alpha,
+            'beta': beta,
+            'clin_papers': len(recs_clin),
+            'var_in_silico': var_in_silico,
+            'func_papers': len(recs_funcs),
+            'var_type': var_type,
+            'unaff': unaff,
+            "alpha_lqt1": alpha_lqt1,
+            "tot_with_prior": tot_with_prior,
+        },
+    )
--- scn5a/views.py
+++ scn5a/views.py
@@ -1,5 +1,6 @@
 import math
-from django.utils import timezone
+
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from scn5a.models import newVariant_scn5a, scn5aDistances, scn5aPapers
@@ -9,32 +10,63 @@ from scn5a.models import newVariant_scn5a, scn5aDistances, scn5aPapers
 # thirty days 86400 * 30
 @cache_page(60*60*31)
 def display(request):
-    recs = newVariant_scn5a.objects.all()
+    recs = newVariant_scn5a.objects.only(
+        'pos',
+        'hgvsc',
+        'var',
+        'resnum',
+        'lqt3',
+        'brs1',
+        'total_carriers',
+        'function',
+        'structure_lqt3',
+        'lqt3_penetrance',
+        'brs1_penetrance',
+    )
     ret = render(request, 'scn5a/main.html', {'recs': recs})
     return ret
 
 
 @cache_page(60*60*31)
 def variantview(request, hgvsc):
-    resnum = newVariant_scn5a.objects.filter(hgvsc=hgvsc).values_list('resnum', flat=True)
-    var = newVariant_scn5a.objects.filter(hgvsc=hgvsc).values_list('var', flat=True)
-    recs_dists = scn5aDistances.objects.filter(resnum=int(resnum[0]), distance__lt=15)
-    neighbors = scn5aDistances.objects.filter(resnum=int(resnum[0])).values_list('neighbor', flat=True)
-    recs_funcs = set(scn5aPapers.objects.filter(variant=var[0], vhalf_act__isnull=False) |
-                     scn5aPapers.objects.filter(variant=var[0], peak__isnull=False) |
-                     scn5aPapers.objects.filter(variant=var[0], late__isnull=False)
-                     )
-    recs_clin = scn5aPapers.objects.filter(variant=var[0], total_carriers__gt=0)
-    recs_vars = set(newVariant_scn5a.objects.filter(resnum__in=neighbors, lqt3__gt=0) |
-                    newVariant_scn5a.objects.filter(resnum__in=neighbors, unaff__gt=0) |
-                    newVariant_scn5a.objects.filter(resnum__in=neighbors, brs1__gt=0) |
-                    newVariant_scn5a.objects.filter(resnum__in=neighbors, gnomad__gt=0)
-                    )
-    variant = newVariant_scn5a.objects.get(hgvsc=hgvsc)
-    if variant.mutAA == "X" or variant.mutAA == "fsX":
-        var_type = False
-    else:
-        var_type = True
+    variant = newVariant_scn5a.objects.select_related(None).only(
+        'resnum',
+        'var',
+        'mutAA',
+        'total_carriers',
+        'alpha_brs1',
+        'alpha_lqt3',
+        'lqt3',
+        'brs1',
+        'beta_brs1',
+        'beta_lqt3',
+        'unaff',
+        'gnomad',
+        'structure_brs1',
+        'structure_lqt3',
+        'pph2_prob',
+        'provean_score',
+        'blast_pssm',
+        'revel_score',
+        'lqt3_dist',
+        'brs1_dist',
+    ).get(hgvsc=hgvsc)
+
+    recs_dists = scn5aDistances.objects.filter(resnum=variant.resnum, distance__lt=15)
+    neighbors = list(
+        scn5aDistances.objects.filter(resnum=variant.resnum).values_list('neighbor', flat=True)
+    )
+    recs_funcs = scn5aPapers.objects.filter(variant=variant.var).filter(
+        Q(vhalf_act__isnull=False) | Q(peak__isnull=False) | Q(late__isnull=False)
+    )
+    recs_clin = scn5aPapers.objects.filter(variant=variant.var, total_carriers__gt=0)
+    recs_vars = newVariant_scn5a.objects.filter(resnum__in=neighbors).filter(
+        Q(lqt3__gt=0) | Q(unaff__gt=0) | Q(brs1__gt=0) | Q(gnomad__gt=0)
+    )
+
+    var_type = not (variant.mutAA in {"X", "fsX"})
+
+    beta = "NA"
     try:
         alpha_brs1 = math.floor(float(variant.alpha_brs1))
         beta_brs1 = 10 - alpha_brs1
@@ -45,7 +77,7 @@ def variantview(request, hgvsc):
         tot_with_prior_lqt3 = 10 + variant.total_carriers
         alpha_brs1_tot = alpha_brs1 + variant.brs1
         tot_with_prior_brs1 = 10 + variant.total_carriers
-    except ValueError and TypeError:
+    except (ValueError, TypeError):
         alpha_brs1 = "NA"
         beta_brs1 = "NA"
         alpha_lqt3 = "NA"
@@ -54,12 +86,28 @@ def variantview(request, hgvsc):
         tot_with_prior_lqt3 = "NA"
         alpha_brs1_tot = "NA"
         tot_with_prior_brs1 = "NA"
-    return render(request, 'scn5a/detail.html', {'recs_dists': recs_dists, 'recs_funcs': recs_funcs,
-                                           'recs_clin': recs_clin, 'var': var, 'recs_vars': recs_vars,
-                                           'variant': variant, 'beta': beta,
-                                           'alpha_brs1': alpha_brs1, 'beta_brs1': beta_brs1, 'alpha_lqt3': alpha_lqt3,
-                                           'beta_lqt3': beta_lqt3, 'clin_papers': len(recs_clin),
-                                           'func_papers': len(recs_funcs), 'var_type': var_type,
-                                                 "alpha_lqt3_tot":alpha_lqt3_tot, "tot_with_prior_lqt3":tot_with_prior_lqt3,
-                                                 "alpha_brs1_tot":alpha_brs1_tot, "tot_with_prior_brs1":tot_with_prior_brs1
-                                                 })
+
+    return render(
+        request,
+        'scn5a/detail.html',
+        {
+            'recs_dists': recs_dists,
+            'recs_funcs': recs_funcs,
+            'recs_clin': recs_clin,
+            'var': [variant.var],
+            'recs_vars': recs_vars,
+            'variant': variant,
+            'beta': beta,
+            'alpha_brs1': alpha_brs1,
+            'beta_brs1': beta_brs1,
+            'alpha_lqt3': alpha_lqt3,
+            'beta_lqt3': beta_lqt3,
+            'clin_papers': len(recs_clin),
+            'func_papers': len(recs_funcs),
+            'var_type': var_type,
+            "alpha_lqt3_tot": alpha_lqt3_tot,
+            "tot_with_prior_lqt3": tot_with_prior_lqt3,
+            "alpha_brs1_tot": alpha_brs1_tot,
+            "tot_with_prior_brs1": tot_with_prior_brs1,
+        },
+    )
